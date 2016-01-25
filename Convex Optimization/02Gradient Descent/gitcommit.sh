f22ad5866f527448e68568cfb322aef24cf57cd4
Saul Garcia <abraham.sagc@gmail.com>
2016-01-25 11:01:12 +0100

    Commit Date Mon Jan 25 11:01:05 CET 2016

--- Convex Optimization/02Gradient Descent/gitcommit.sh
+++
@@ -1,7 +0,0 @@
-INPUTDATE=`date`
-git pull
-git add .
-git reset -- gitcommit.sh
-git commit -m "Commit Date $INPUTDATE"
-git remote add origin git@github.com:Saulabrm/Python-Optimization.git
-git push -u origin master
